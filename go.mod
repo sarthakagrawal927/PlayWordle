4f333b66d753c8446db2680636beb382ccaf4950
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-01-23 22:51:11 +0530

    Add command line mode to test a word

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sarthakagrawal927/playwordle.git
+
+go 1.19
+
+require github.com/hellflame/argparse v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,42 +1,57 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hellflame/argparse"
+)
+
 func main() {
 	// Code to play the game
-	// var status string
-	// var currWord string
-	// reader := bufio.NewReader(os.Stdin)
-	// currWordCount := int(0)
-	// copy(currentWords, wordList)
-	// for status != "WIN" {
-	// 	findAlphabetsFrequency()
-	// 	currWord = getMostProbableWord(currWordCount)
-	// 	fmt.Println(currWord, len(currentWords), characterCountMap)
-	// 	input, err := reader.ReadString('\n')
-	// 	if err != nil {
-	// 		fmt.Println("An error occurred while reading input. Please try again", err)
-	// 		return
-	// 	}
-	// 	status = strings.ToUpper(strings.TrimSuffix(input, "\n"))
-	// 	moves := makePlayObject(currWord, status)
-	// 	currWordCount += 1
-	// 	playMoves(moves)
-	// }
-
-	// Code to simulate the game
-
-	// fmt.Println(simulate("APPLE"), len(currentWords), len(wordList))
-	// fmt.Println(simulate("ADZED"), len(currentWords), len(wordList))
-	// fmt.Println(simulate("HEVEA"), len(currentWords), len(wordList))
-	// fmt.Println(simulate("YRIVD"), len(currentWords), len(wordList))
-	// moves := make(chan int, 1)
-	// simulate("ZULUS", moves)
-	// fmt.Println(<-moves)
-	simulateForAll()
-
-	// fmt.Println(currentWords)
+	var status string
+	var currWord string
+	reader := bufio.NewReader(os.Stdin)
+	currWordCount := int(0)
+	copy(currentWords, wordList)
+	for status != "WIN" {
+		findAlphabetsFrequency()
+		currWord = getMostProbableWord(currWordCount)
+		fmt.Println(currWord, len(currentWords), characterCountMap)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occurred while reading input. Please try again", err)
+			return
+		}
+		status = strings.ToUpper(strings.TrimSuffix(input, "\n"))
+		moves := makePlayObject(currWord, status)
+		currWordCount += 1
+		playMoves(moves)
+	}
 }
 
-var showDebug bool = true
+func isWordPlayable() {
+	parser := argparse.NewParser("wordle", "Add word", nil)
+	word := parser.String("w", "word", nil)
+
+	err := parser.Parse(nil)
+	if err != nil {
+		return
+	}
+	if len(*word) != 5 {
+		fmt.Println("Please add 5 letter word")
+		return
+	}
+	moves := make(chan int, 1)
+	findAlphabetsFrequency()
+
+	simulate(*word, moves)
+	fmt.Println(<-moves, *word)
+}
+
+var showDebug bool = false
 
 func init() {
 	initWordsArray()
